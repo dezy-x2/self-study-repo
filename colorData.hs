f699875c7e2c13169acbce9c497ddf1d168a6110
Daniel Desmond <df.dezmond9@gmail.com>
2021-05-21 13:23:14 -0400

    reformatted to use RGB instead of RBY

--- colorData.hs
+++ colorData.hs
@@ -1,34 +1,34 @@
-data Color = Blue | Yellow | Red | Mix Color Color
+data Color = Blue | Green | Red | Mix Color Color
     deriving(Show)
 
 blue = Mix Blue Blue
-yellow = Mix Yellow Yellow
+green = Mix Green Green
 red = Mix Red Red
-orange = Mix Red Yellow
-green = Mix Blue Yellow
-violet = Mix Blue Red
+yellow = Mix Red Green
+cyan = Mix Blue Green
+magenta = Mix Blue Red
 
 instance Ord Color where
     compare Blue (Mix x y) = GT
     compare (Mix x y) Blue = LT
     compare Red (Mix x y) = GT
     compare (Mix x y) Red = LT
-    compare Yellow (Mix x y) = GT
-    compare (Mix x y) Yellow = LT
+    compare Green (Mix x y) = GT
+    compare (Mix x y) Green = LT
     compare (Mix x1 x2) (Mix y1 y2) = compare x1 y1
     compare Red Red = EQ 
-    compare Yellow Yellow = EQ
+    compare Green Green = EQ
     compare Blue Blue = EQ 
-    compare Red Yellow = EQ
-    compare Yellow Red = EQ
-    compare Blue Yellow = EQ
-    compare Yellow Blue = EQ
+    compare Red Green = EQ
+    compare Green Red = EQ
+    compare Blue Green = EQ
+    compare Green Blue = EQ
     compare Red Blue = EQ
     compare Blue Red = EQ
 
 instance Eq Color where
     Red == Red = True
-    Yellow == Yellow = True
+    Green == Green = True
     Blue == Blue = True
     Mix x1 x2 == Mix y1 y2 = ((x1 == y1) && (x2 == y2)) || ((x1 == y2) && (x2 == y1))
     x == y = False
@@ -40,11 +40,12 @@ instance Num Color where
     --TODO: figure out what subtraction should do
     --TODO: like how do you subract colors when they are set up like this?
 
+    --TODO: use fromInteger to convert primary colors to their wavelengths and then multiply those!!
 
     -- x * y = Rainbow
 
     -- fromInteger 1 = Red
-    -- fromInteger 2 = Yellow
+    -- fromInteger 2 = Green
     -- fromInteger 3 = Blue
     -- fromInteger 4 = Green
     -- fromInteger 5 = Orange
@@ -57,14 +58,14 @@ instance Num Color where
 -- primaryOrNot :: Color -> Bool
 -- primaryOrNot color 
 --     | color == Red = True
---     | color == Yellow = True
+--     | color == Green = True
 --     | color == Blue = True
 --     | otherwise = False
 
 -- baseColors :: Color -> Maybe (Color, Color)
 -- baseColors color
---     | color == Green = Just (Yellow, Blue)
---     | color == Orange = Just (Yellow, Red)
+--     | color == Green = Just (Green, Blue)
+--     | color == Orange = Just (Green, Red)
 --     | color == Violet = Just (Blue, Red)
 --     | otherwise = Nothing
 
@@ -73,8 +74,8 @@ instance Num Color where
 -- matchOrNo Red Green = True
 -- matchOrNo Orange Blue = True
 -- matchOrNo Blue Orange = True
--- matchOrNo Yellow Violet = True
--- matchOrNo Violet Yellow = True
+-- matchOrNo Green Violet = True
+-- matchOrNo Violet Green = True
 -- matchOrNo Rainbow Rainbow = True
 -- matchOrNo Rainbow x = True
 -- matchOrNo x Rainbow = True
