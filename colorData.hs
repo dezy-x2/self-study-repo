0d2c10f26b13a8bfdb8e9d44ea6e4a918eb56485
Daniel Desmond <df.dezmond9@gmail.com>
2021-05-24 09:31:07 -0400

    comments

--- colorData.hs
+++ colorData.hs
@@ -1,6 +1,9 @@
+-- declares the data type for colors and includes a way to make a list of colors
+-- the main colors are supposed to be hidden from the user and the user would only use the colors in the list form
 data Color = Blue | Green | Red | Black | White | Color [Color]
     deriving(Show)
 
+-- preset colors 
 blue = Color [Blue]
 green = Color [Green]
 red = Color [Red]
@@ -8,6 +11,7 @@ yellow = Color [Red, Green]
 cyan = Color [Blue, Green]
 magenta = Color [Blue, Red]
 
+-- this just helps keep the num class less convoluted by putting some helper code out here
 subtractionHelper :: Color -> Color -> Color
 subtractionHelper color1 color2 =   if color1 == color2
                                         then Black
@@ -20,30 +24,31 @@ instance Eq Color where
     Black == Black = True
     White == White = True
     (Color x) == (Color y) = x == y
+    -- this one is the catch all for any other situation
     x == y = False
 
 instance Num Color where
     color1 + color2 = Color $ [color1, color2]
 
-    --TODO: figure out what subtraction should do
-    --TODO: like how do you subract colors when they are set up like this?
-
     (Color (x:xs)) - (Color (y:ys)) = Color $ [(subtractionHelper x y), ((Color xs) - (Color ys))]
+    -- base case for if the pattern doesn't stick
     x - y = x
 
-    --TODO: use fromInteger to convert primary colors to their wavelengths and then multiply those!!
-
+    -- this seemed like the best solution for what multiplying colors does 
     x * y = Color [White]
 
+    -- tests if the number is inbetween a certain wavelength and therefore equivilent to a certain color
     fromInteger num
         | 563 < num && num < 580 = Color [Red]
         | 533 < num && num < 546 = Color [Green]
         | 419 < num && num < 440 = Color [Blue]
         | otherwise = Color [Black]
 
+    -- idk what these would do with colors so they just return the arg
     abs = id
     signum = id
 
+-- returns all of the existing colors in a list that is not typed in the Color type
 baseColors :: Color -> [Color]
 baseColors (Color (x:xs)) = [x, Color (baseColors (Color xs))]
 baseColors (Color []) = []
